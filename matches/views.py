fd00d3f16e2f2f4b0d4b658b23dc9bc37f9e486c
Friendlysiva143 <friendlysiva143@gmail.com>
2025-11-22 20:13:30 +0530

    matches_view

--- matches/views.py
+++ matches/views.py
@@ -1,3 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def active_matches(request):
+    # Query for user's active matches
+    return render(request, 'matches/active.html', { 'active_matches': [...] })
+
+def match_requests(request):
+    # Query for user's match requests
+    return render(request, 'matches/requests.html', { 'match_requests': [...] })
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
