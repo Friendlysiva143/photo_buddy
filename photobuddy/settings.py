0a5e7b8810fa75f5e23adda0d6693cf31bd33485
Friendlysiva143 <friendlysiva143@gmail.com>
2025-11-22 12:22:56 +0530

    apps

--- photobuddy/settings.py
+++ photobuddy/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'chat',
+    'locations',
+    'matches',
 ]
 
 #ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'photobuddy.onrender.com').split(',')
