f11de2cd3190831116db00491876028d2a45a304
Friendlysiva143 <friendlysiva143@gmail.com>
2025-11-22 12:31:03 +0530

    user

--- locations/models.py
+++ locations/models.py
@@ -1,11 +1,11 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 class UserLocation(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username
+        return str(self.user)
