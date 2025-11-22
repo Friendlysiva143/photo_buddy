bbbd570fcd6919a4a50e51c0ef5f44cfc29cbdf3
Friendlysiva143 <friendlysiva143@gmail.com>
2025-11-22 20:19:47 +0530

    matches_urls

--- matches/urls.py
+++ matches/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
    path('request/', views.send_request, name='send_request'),
     path('incoming/', views.incoming_requests, name='incoming_requests'),
     path('respond/', views.respond_request, name='respond_request'),
+    path("active/", views.active_matches, name="active_matches"),
+    path("requests/", views.match_requests, name="match_requests"),
+    
+
 ]
