20fb533a739ac6d3562b47e4920793eb8931a6a2
Friendlysiva143 <friendlysiva143@gmail.com>
2025-11-23 10:24:57 +0530

    user_views

--- matches/views.py
+++ matches/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.contrib.auth.models import User
 
 from .models import MatchRequest, Match
 from chat.models import ChatRoom
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # -----------------------------
 # SEND REQUEST
